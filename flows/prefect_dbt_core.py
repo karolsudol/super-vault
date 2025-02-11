4f59e71bd6edee867907677cb34ebe65113bba65
karol <karol@sudoltrust.com>
2025-02-11 20:36:47 +0000

    prefect-dbt

---
+++ flows/prefect_dbt_core.py
@@ -0,0 +1,15 @@
+
+from prefect_dbt.cli.commands import DbtCoreOperation
+from prefect import flow
+
+@flow
+def trigger_dbt_flow() -> str:
+    result = DbtCoreOperation(
+        commands=["dbt build -t prod"],
+        project_dir="prefect_demo",
+        profiles_dir="~/.dbt"
+    ).run()
+    return result
+
+if __name__ == "__main__":
+    trigger_dbt_flow()
